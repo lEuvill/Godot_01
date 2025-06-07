8fb5b7e7fe74ffeb5bbad0609e2c24b893921a43
lEuvill <jpantas.nextvas@gmail.com>
2025-06-08 04:19:14 +0800

    added pathfind

--- Scripts/supply.gd
+++ Scripts/supply.gd
@@ -2,11 +2,24 @@ extends Area2D
 
 @onready var game_manager: GameManager = %Game_Manager
 
+@onready var shop_ui: Panel = $Panel
+
+func _ready() -> void:
+	shop_ui.visible = false
+	
+	
 
 func _on_body_entered(body: Node2D) -> void:
-	var rice_data = game_manager.get_item("Rice")
-	print(rice_data["count"])  # prints 10
-	print(rice_data["price"])  # prints 100 (if quality is High)
+	if body.name == "p1":
+		var rice_data = game_manager.get_item("Rice")
+		print(rice_data["count"])  # prints 10
+		print(rice_data["price"])  # prints 100 (if quality is High)
 
-	print("Interact")
-	
+		
+		shop_ui.visible = true
+		print("body entered ",str(shop_ui))
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "p1":
+		shop_ui.visible = false
+		print("body exited ",str(shop_ui))
